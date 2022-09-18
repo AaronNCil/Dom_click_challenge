114e6d958e322d5f2422341dde8b90766687218b
Aaron Cil <cilaaron27@gmail.com>
2022-09-17 23:33:26 -0400

    first commit

---
+++ InClassProblem/app.js
@@ -0,0 +1,18 @@
+function dontcopy() {
+    console.log('hi');
+    document.querySelector(".mainNum").innerHTML= 12;
+}
+
+
+
+function upNum() {
+    console.log('sup');
+    document.querySelector(".mainNum").innerHTML++;
+    
+}
+
+function downNum() {
+    console.log('bye');
+    document.querySelector(".mainNum").innerHTML--;
+
+}
---
+++ app.js
@@ -0,0 +1,8 @@
+function blue() {
+    console.log('this works');
+    document.querySelector(".ninja").style.color="blue";
+}
+
+function bigger() {
+    document.querySelector(".btn").style.fontSize="50px";
+}
